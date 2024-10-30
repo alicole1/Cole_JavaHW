c9bd4eef926153989f2ec1c79a82a83a5c53a6df
Aliboop <cole24793@gmail.com>
2024-10-30 19:55:29 -0400

    Unit 04: AT06

--- Unit04/AT06/Calender.java
+++ Unit04/AT06/Calender.java
@@ -5,92 +5,98 @@ import java.time.Month;
 
 public class Calender {
     public static void main(String[] args) {
-        // Collect Month
+        // Set a variable to collect the month and date it is today.
         LocalDate today = LocalDate.now();
-        Month currentMonth = today.getMonth();
-        String currentMonthString = currentMonth.toString();
-        int monthLength = currentMonthString.length();
 
+
+        /*
+        * SETTING AND DISPLAYING UP HEADER (month and days of week).
+        */
+        // Use declared variable to get the current month.
+        Month todayMonth = today.getMonth();
+
+        // Center the month title on the page.
+        String currentMonthString = todayMonth.toString(); // Needs to be a string to determine length.
+        int monthLength = currentMonthString.length(); // Find how many characters make up the name of this month.
         int spaces = (28 - monthLength) / 2;
-
+            // Whole row is 28 spaces across. x number of these spaces are taken up by month name. 
+            // Left with x spaces padding name, divide by 2 to determine how many are on the right.
         for (int i = 0; i < spaces; i++) {
             System.out.print(" ");
-        }
+        } // Loop through printing spaces unitl we've reached the number we want.
+        System.out.println(todayMonth); // Print the month name after the spaces.
 
-        System.out.println(currentMonth);
+        // Print days of week.
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
 
-        LocalDate firstOfMonth = today.minusDays(today.getDayOfMonth() - 1);
-        // System.out.println(firstOfMonth);
 
-        DayOfWeek firstDayOfMonth = firstOfMonth.getDayOfWeek();
-        // System.out.println(firstDayOfMonth);
+        /* 
+        * SETTING UP START OF "ARRAY".
+        */
+        // Find information about first day of the month.
+        LocalDate firstOfMonth = today.minusDays(today.getDayOfMonth() - 1); // Declare new variable for the first day of the month.
+        DayOfWeek firstDayOfMonth = firstOfMonth.getDayOfWeek(); // Using declared variable, find what day of the week this date was on.
 
-        int currentNumInRow = 0;
+        int currentNumInRow = 0; // Create new counter variable for the future to track how many dates are in our row already.
 
         switch (firstDayOfMonth) {
-            case MONDAY:
-                spaces = 0;
-                break;
-            case TUESDAY:
-                spaces = 4;
-                currentNumInRow = 1;
-                break;
+            case MONDAY: // If first day of month is on monday...
+                spaces = 0; // Start on first of row. 
+                break; // First date will be first date in row, so no need to add one to counter.
+            case TUESDAY: // If first date of month is on tuesday...
+                spaces = 4; // Start further in to the "array"
+                break; // Monday will be tracked at start of row; so no need to add one to counter.
             case WEDNESDAY:
                 spaces = 8;
-                currentNumInRow = 2;
-                break;
+                currentNumInRow = 1; // Monday will NOT be tracked when we start on wednesday, so we need to add one to counter to account for it.
+                break; 
             case THURSDAY:
                 spaces = 12;
-                currentNumInRow = 3;
+                currentNumInRow = 2; // Now Monday nor Tuesday will be tracked, so we now need to add two to counter.
                 break;
             case FRIDAY:
                 spaces = 16;
-                currentNumInRow = 4;
+                currentNumInRow = 3;
                 break;
             case SATURDAY:
                 spaces = 20;
-                currentNumInRow = 5;
+                currentNumInRow = 4;
                 break;
             case SUNDAY:
                 spaces = 24;
-                currentNumInRow = 6;
+                currentNumInRow = 5;
                 break;
         }
-
         for (int i = 0; i < spaces; i++) {
             System.out.print(" ");
-        }
+        } // Add the number of spaces declared in switch statement above to properly position the first date.
 
-        int maxDays = currentMonth.length(false);
-        int date = 1;
+        /*
+         * DISPLAYING NUMBERS AND DATES.
+         */
 
-        for (int i = 0; i < maxDays; i++) {
-            System.out.printf(" %2d", date);
-            if (date == today.getDayOfMonth()) {
-                System.out.print("*");
+        // Find max number of days in this month.
+        int maxDays = todayMonth.length(false);
+
+        // Loop to add dates until we get to max days in the month.
+        for (int i = 1; i < maxDays + 1; i++) {
+            currentNumInRow = currentNumInRow + 1; // Add one more "tracked" number in the row.
+            if (currentNumInRow == 7) { // If we have 7 numbers in our row... 
+                System.out.println(""); // Start the next row.
+                currentNumInRow = 0; // Reset counter.
+            }
+            
+            System.out.printf(" %2d", i); // Print a space, then the number. if number is one digit, add a space before it.
+            if (i == today.getDayOfMonth()) { // If the date is the day it is today...
+                System.out.print("*"); // Add a star next to it.
             } else {
-                System.out.print(" ");
+                System.out.print(" "); // Otherwise, add a space.
             }
-            if (date == maxDays) {
-                System.out.println("");
+            if (i == maxDays) {
+                System.out.println(""); // If it's the last day of the month, create new line.
             }
-            date = date + 1;
-            currentNumInRow = currentNumInRow + 1;
-            if (currentNumInRow == 7) {
-                System.out.println("");
-                currentNumInRow = 0;
-            } else {
-              //  System.out.print(" ");
-            }
+            
         }
-
-
-
-
-        
-
-        
 
     }
 }
