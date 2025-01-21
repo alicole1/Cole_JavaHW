0c00b95fe43f3b3f794ed55b521d1d79204ac337
Aliboop <cole24793@gmail.com>
2025-01-21 13:29:59 -0500

    final! done! practical portion of quiz

--- Practical Exams/January21.25/PartA.java
+++ Practical Exams/January21.25/PartA.java
@@ -1,21 +1,31 @@
-
 import java.util.Arrays;
 
 public class PartA {
+
+    /*
+     * method that swaps the values in the array.
+     * takes the minimum position, the maximum position, and an array of ints as
+     * arguments.
+     */
     public static void swap(int min, int max, int[] array) throws Exception {
         try {
+            // swaps the two numbers at the positions specified.
             int placeholder = array[min];
             array[min] = array[max];
             array[max] = placeholder;
         } catch (Exception e) {
+            // throws an exception if the min / max falls outside the range.
             System.err.println("Oops! Specified integers are outside of range. No swap was done. ");
         }
 
     }
 
     public static void main(String[] args) throws Exception {
+        // declares and initializes array of ints.
         int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        // calls method.
         swap(2, 7, numbers);
+        // prints new array to terminal.
         System.out.println(Arrays.toString(numbers));
     }
 }
--- Practical Exams/January21.25/PartB.java
+++ Practical Exams/January21.25/PartB.java
@@ -2,20 +2,52 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PartB {
-    public static void fillArray(Scanner scan, ArrayList<Double> scores) {
-        String input = "";
-        while (! input.equals("q")) { 
-            System.out.print("Enter test scores");
-            
+
+    /**
+     * Method: fills an array with test scores. returns the average of all scores
+     * entered
+     */
+    public static double fillArray(Scanner scan, ArrayList<Double> scores) {
+        // declares variables to be called.
+        double score; // stores current score entered.
+        double total = 0; // stores total of scores. (for avg calc)
+        int scoresAdded = 0; // stores total numbers of scores. (for avg calc)
+        double average; // will store final average.
+        String input = ""; // will store input.
+        while (true) {
+            System.out.print("Enter a quiz score or 'q' to quit: ");
+
             if (scan.hasNextDouble()) {
-                input = scan.nextDouble();
-                if ()
+                // add score to array and update doubles.
+                score = scan.nextDouble();
+                total = total + score;
+                scores.add(score);
+                scoresAdded++;
+            } else if (scan.hasNext()) {
+                // check if user wants to quit.
+                input = scan.next();
+                if (input.toLowerCase().equals("q")) {
+                    average = total / scoresAdded;
+                    return average;
+                }
             }
         }
-
     }
 
     public static void main(String[] args) throws Exception {
+        // declare and initialize scan and arraylist objects.
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Double> scores = new ArrayList<>();
 
+        // call method.
+        double average = fillArray(scan, scores);
+
+        // display new array and average to user in terminal.
+        System.out.println("-------------------");
+        for (int i = 0; i < scores.size(); i++) {
+            System.out.printf("%8.2f %n", scores.get(i));
+        }
+        System.out.println("-------------------");
+        System.out.printf("avg = %.2f %n", average);
     }
 }
