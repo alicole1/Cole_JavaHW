6af1fc92db7cb223472e5e3f0d0ff414dd9bc152
Aliboop <cole24793@gmail.com>
2025-03-03 17:44:52 -0500

    animate test trial
    Rotation off center still :/

---
+++ Animation/A01/animate/Board.java
@@ -0,0 +1,59 @@
+package animate;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int SIDELENGTH = 150;
+
+    /*
+     * Constructor
+     */
+    public Board() {
+        setBackground(Color.CYAN);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        int x_t = getWidth() / 2;
+        int y_t = getHeight() / 2;
+        x_t = x_t - SIDELENGTH / 2;
+        y_t = y_t - SIDELENGTH / 2;
+
+        Rectangle r = new Rectangle(0, 0, SIDELENGTH, SIDELENGTH);
+
+        // shift or translate the magenta square from (0,0) to center.
+        AffineTransform af = new AffineTransform();
+        af.translate(x_t, y_t);
+
+        double theta = Math.toRadians(22.5);
+        double anchorx = x_t + SIDELENGTH / 2;
+        double anchory = y_t + SIDELENGTH / 2;
+
+        af.rotate(theta, anchorx, anchory);
+
+        Shape shape = af.createTransformedShape(r);
+        g2d.setColor(Color.MAGENTA);
+        g2d.fill(shape);
+
+        g2d.setColor(Color.BLACK);
+        g2d.fillOval((int) anchorx, (int) anchory, 3, 3);
+
+        // send a little message.
+        g2d.setColor(Color.BLUE);
+        g2d.drawString("<333", 50, 50);
+    }
+
+}
