a7c632c368a506add462649b3d8fbc910cf08ab6
Aliboop <cole24793@gmail.com>
2025-03-31 14:12:31 -0400

    Added lesson 33

---
+++ Animation/Lesson33/Demo.java
@@ -0,0 +1,20 @@
+public class Demo {
+    public static void main(String[] args) {
+        Animal elephant = new Animal("Tank", 10000);
+        Animal ant = new Animal("Pipsqueak", 1);
+
+        switch (ant.compareTo(elephant)) {
+            case -1:
+                System.out.println(ant.getName() + " has a smaller weight than " + elephant.getName());
+                break;
+            case 0:
+                System.out.println(ant.getName() + " has the same weight as " + elephant.getName());
+                break;
+            case 1:
+                System.out.println(ant.getName() + " has a larger weight than " + elephant.getName());
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+}
