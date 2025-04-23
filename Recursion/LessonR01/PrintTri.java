e68cfc295a2f93280a8ae102828cb2a2e5c22781
Aliboop <cole24793@gmail.com>
2025-04-22 22:00:43 -0400

    R01

--- Recursion/LessonR01/PrintTri.java
+++ Recursion/LessonR01/PrintTri.java
@@ -1,18 +1,17 @@
 public class PrintTri {
-    public static void printTri(int n) {
-        int currStars = 1;
-        if (currStars == n) {
-            for (int i = 0; i < n; i++) {
-                System.out.print("*");
-            }
-            System.out.printf("%n");
-        } else {
-            for (int i = 0; i < currStars; i++) {
-                System.out.print("*");
-            }
-            System.out.printf("%n");
-            printTri(currStars + 1);
+    public static void printTriHelper(int finalCount, int currCount) {
+        if (currCount > finalCount) {
+            return;
         }
+        for (int i = 0; i < currCount; i++) {
+            System.out.print("*");
+        }
+        System.out.printf("%n");
+        printTriHelper(finalCount, currCount + 1);
+    }
+
+    public static void printTri(int n) {
+        printTriHelper(n, 1);
     }
 
     public static void main(String[] args) {
